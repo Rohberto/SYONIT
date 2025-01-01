5501cbc5ea70888efc599ca3164dc6c71444d8c9
Rohberto <oluwaseunrobert44@gmail.com>
2025-01-01 08:39:04 +0100

    Tourna-musement

--- src/app/Components/Header.js
+++ src/app/Components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="header">
-    <h1>SYONIT TOURNA-MAZEMENT</h1>
+    <h1>SYONIT TOURNA-MUSEMENT</h1>
   </div>
   )
 }
