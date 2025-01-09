810e6370ce2842f5297c564c63f463eca81fe17e
Rohberto <oluwaseunrobert44@gmail.com>
2025-01-09 07:56:49 +0100

    paying attention to details

--- src/app/Components/Onboarding/Onboarding.jsx
+++ src/app/Components/Onboarding/Onboarding.jsx
@@ -23,7 +23,7 @@ const Onboarding = () => {
 
             <div className="new_onboarding_bottom_content">
                 
-             <img src='/syonit_musketeer.png' alt='musketeeers' className="onboarding_musketeer"/>
+             <img src='/main_musketeer.svg' alt='musketeeers' className="onboarding_musketeer"/>
              
              <div className="new_onboarding_bottom_text">
                 <p className="think_differently">THINK DIFFERENTLY</p>
