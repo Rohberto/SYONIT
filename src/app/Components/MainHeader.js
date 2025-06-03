57cf6fec611b7f077ff9576d911da92ad6c8c4a0
Rohberto <oluwaseunrobert44@gmail.com>
2025-06-03 14:54:38 +0100

    invite

--- src/app/Components/MainHeader.js
+++ src/app/Components/MainHeader.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="header_section">
                 <div className="profile_icon"><FaRegUser/><span className='online'></span></div>
-                <h1 className="header">SYONIT</h1>
+                <h1 className="headerr">SYONIT</h1>
                 <div className="book_icon"><BsBook/></div>
               </div>
     )
